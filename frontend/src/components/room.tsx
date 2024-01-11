9317b27e0090154fb991c8fe69ad8ede8e408285
Sunil <sunisimar2002d@gmail.com>
2024-01-11 21:40:57 +0530

    adding room component

--- frontend/src/components/room.tsx
+++ frontend/src/components/room.tsx
@@ -1,10 +1,53 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
+import { Socket, io } from "socket.io-client";
+
+const URL = "http://localhost:3000";
 
 export const Room = () => {
   const [searchParams, setSearchParams] = useSearchParams("");
   const name = searchParams.get("name");
+  const [socket, setSocket] = useState<null | Socket>(null);
+  const [lobby, setLobby] = useState(true);
 
-  useEffect(() => {}, [name]);
-  return <div>hi {name} </div>;
+  useEffect(() => {
+    const socket = io(URL);
+    socket.on("send-offer", ({ roomId }) => {
+      setLobby(false);
+      socket.emit("offer", {
+        sdp: "",
+        roomId,
+      });
+    });
+
+    socket.on("offer", ({ roomId, offer }) => {
+      setLobby(false);
+      socket.emit("answer", {
+        roomId,
+        sdp: "",
+      });
+    });
+
+    socket.on("answer", ({ roomId, answer }) => {
+      setLobby(false);
+    });
+
+    socket.on("lobby", () => {
+      setLobby(true);
+    });
+
+    setSocket(socket);
+  }, [name]);
+
+  if (lobby) {
+    return <div>Waiting to Connect you to Someone</div>;
+  }
+
+  return (
+    <div>
+      hi {name}
+      <video width={400} height={400} />
+      <video width={400} height={400} />
+    </div>
+  );
 };
